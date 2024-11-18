f6181657f4146cc5e9ebdeedeb69f9ebaabb7f1f
FA <fabricecoder009@gmail.com>
2024-11-18 18:00:09 +0200

    creation and initialisation of order model class

--- src/main/java/com/inventorysystem/model/Order.java
+++ src/main/java/com/inventorysystem/model/Order.java
@@ -1,4 +1,51 @@
 package com.inventorysystem.model;
 
+import jakarta.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "Order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderId;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date orderDate;
+
+    @ManyToOne
+    @JoinColumn(name = "providerId", nullable = false)
+    private Provider provider;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderDetails> orderDetails;
+
+    // Getters and setters...
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
 }
+
