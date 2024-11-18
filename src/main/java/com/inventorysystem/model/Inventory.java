f881c2e8a5b281d2c6b00b14891428b0fd19fc2a
FA <fabricecoder009@gmail.com>
2024-11-18 19:54:05 +0200

    creation and initialisation of inventory, location and warehouse model classes

--- src/main/java/com/inventorysystem/model/Inventory.java
+++ src/main/java/com/inventorysystem/model/Inventory.java
@@ -1,4 +1,78 @@
 package com.inventorysystem.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Inventory")
 public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int inventoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "productId", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantityAvailable;
+
+    @Column(nullable = false)
+    private int minimumStockLevel;
+
+    @Column(nullable = false)
+    private int maximumStockLevel;
+
+    @Column(nullable = false)
+    private int reorderPoint;
+
+    // Getters and setters...
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public int getMinimumStockLevel() {
+        return minimumStockLevel;
+    }
+
+    public void setMinimumStockLevel(int minimumStockLevel) {
+        this.minimumStockLevel = minimumStockLevel;
+    }
+
+    public int getMaximumStockLevel() {
+        return maximumStockLevel;
+    }
+
+    public void setMaximumStockLevel(int maximumStockLevel) {
+        this.maximumStockLevel = maximumStockLevel;
+    }
+
+    public int getReorderPoint() {
+        return reorderPoint;
+    }
+
+    public void setReorderPoint(int reorderPoint) {
+        this.reorderPoint = reorderPoint;
+    }
 }
+
--- src/main/java/com/inventorysystem/model/Warehouse.java
+++ src/main/java/com/inventorysystem/model/Warehouse.java
@@ -1,4 +1,55 @@
 package com.inventorysystem.model;
 
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "Warehouse")
 public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int warehouseId;
+
+    @Column(nullable = false, length = 100)
+    private String warehouseName;
+
+    @Column(nullable = false)
+    private boolean isRefrigerated;
+
+    @OneToMany(mappedBy = "warehouse", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Inventory> inventories;
+
+    // Getters and setters...
+
+    public int getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(int warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    public String getWarehouseName() {
+        return warehouseName;
+    }
+
+    public void setWarehouseName(String warehouseName) {
+        this.warehouseName = warehouseName;
+    }
+
+    public boolean isRefrigerated() {
+        return isRefrigerated;
+    }
+
+    public void setRefrigerated(boolean refrigerated) {
+        isRefrigerated = refrigerated;
+    }
+
+    public Set<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public void setInventories(Set<Inventory> inventories) {
+        this.inventories = inventories;
+    }
 }
