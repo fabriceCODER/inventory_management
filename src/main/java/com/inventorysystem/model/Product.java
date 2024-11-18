6d2abebd755429c3d042d65a3c8cbbb9a99a698d
FA <fabricecoder009@gmail.com>
2024-11-18 15:38:08 +0200

    creation and initialisation of of product model class

--- src/main/java/com/inventorysystem/model/Product.java
+++ src/main/java/com/inventorysystem/model/Product.java
@@ -1,4 +1,88 @@
 package com.inventorysystem.model;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    @Column(nullable = false, unique = true)
+    private String productCode;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(length = 2000)
+    private String productDescription;
+
+    @Column(nullable = false)
+    private String productCategory;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal packedWeight;
+
+    @Column(nullable = false)
+    private boolean refrigerated;
+
+    // Getters and setters...
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public BigDecimal getPackedWeight() {
+        return packedWeight;
+    }
+
+    public void setPackedWeight(BigDecimal packedWeight) {
+        this.packedWeight = packedWeight;
+    }
+
+    public boolean isRefrigerated() {
+        return refrigerated;
+    }
+
+    public void setRefrigerated(boolean refrigerated) {
+        this.refrigerated = refrigerated;
+    }
 }
