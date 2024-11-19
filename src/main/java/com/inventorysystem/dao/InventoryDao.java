ccd3be476ca5c571268f14b845fd4e1289460224
FA <fabricecoder009@gmail.com>
2024-11-19 11:59:20 +0200

    init of InventoryDao class

--- src/main/java/com/inventorysystem/dao/InventoryDao.java
+++ src/main/java/com/inventorysystem/dao/InventoryDao.java
@@ -1,4 +1,63 @@
 package com.inventorysystem.dao;
 
-public class InventoryDao {
+import com.inventorysystem.model.Inventory;
+import com.inventorysystem.util.BaseDao;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class InventoryDao extends BaseDao {
+
+    public List<Inventory> getAllInventories() {
+        try (Session session = getSession()) {
+            return session.createQuery("from Inventory", Inventory.class).list();
+        }
+    }
+
+    public Inventory getInventory(int id) {
+        try (Session session = getSession()) {
+            return session.get(Inventory.class, id);
+        }
+    }
+
+    public void saveInventory(Inventory inventory) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            session.save(inventory);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
+
+    public void updateInventory(Inventory inventory) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            session.update(inventory);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteInventory(int id) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            Inventory inventory = session.get(Inventory.class, id);
+            if (inventory != null) {
+                session.delete(inventory);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
 }
+
