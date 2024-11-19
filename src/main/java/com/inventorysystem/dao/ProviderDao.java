358a49dca4d0bbce1737f81f67622c316b70ad82
FA <fabricecoder009@gmail.com>
2024-11-19 10:18:28 +0200

    creation and initialisation of CProviderDao class

--- src/main/java/com/inventorysystem/dao/ProviderDao.java
+++ src/main/java/com/inventorysystem/dao/ProviderDao.java
@@ -1,4 +1,63 @@
 package com.inventorysystem.dao;
 
-public class ProviderDao {
+
+import com.inventorysystem.model.Provider;
+import com.inventorysystem.util.BaseDao;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class ProviderDao extends BaseDao {
+
+    public List<Provider> getAllProviders() {
+        try (Session session = getSession()) {
+            return session.createQuery("from Provider", Provider.class).list();
+        }
+    }
+
+    public Provider getProvider(int id) {
+        try (Session session = getSession()) {
+            return session.get(Provider.class, id);
+        }
+    }
+
+    public void saveProvider(Provider provider) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            session.save(provider);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
+
+    public void updateProvider(Provider provider) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            session.update(provider);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteProvider(int id) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            Provider provider = session.get(Provider.class, id);
+            if (provider != null) {
+                session.delete(provider);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
 }
