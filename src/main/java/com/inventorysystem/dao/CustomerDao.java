13286fba5fe8444935f2ae461bd0288969aff296
FA <fabricecoder009@gmail.com>
2024-11-19 10:15:25 +0200

    creation and initialisation of CustomerDao class

--- src/main/java/com/inventorysystem/dao/CustomerDao.java
+++ src/main/java/com/inventorysystem/dao/CustomerDao.java
@@ -1,4 +1,62 @@
 package com.inventorysystem.dao;
 
-public class CustomerDao {
+import com.inventorysystem.model.Customer;
+import com.inventorysystem.util.BaseDao;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class CustomerDao extends BaseDao {
+
+    public List<Customer> getAllCustomers() {
+        try (Session session = getSession()) {
+            return session.createQuery("from Customer", Customer.class).list();
+        }
+    }
+
+    public Customer getCustomer(int id) {
+        try (Session session = getSession()) {
+            return session.get(Customer.class, id);
+        }
+    }
+
+    public void saveCustomer(Customer customer) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            session.save(customer);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
+
+    public void updateCustomer(Customer customer) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            session.update(customer);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteCustomer(int id) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            Customer customer = session.get(Customer.class, id);
+            if (customer != null) {
+                session.delete(customer);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            e.printStackTrace();
+        }
+    }
 }
