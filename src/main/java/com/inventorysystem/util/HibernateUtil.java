f05319728180b7d915f0e2469fa89c2633429fb3
FA <fabricecoder009@gmail.com>
2024-11-19 13:00:48 +0200

    Creation and Initialisation of HibernateUtil class

--- src/main/java/com/inventorysystem/util/HibernateUtil.java
+++ src/main/java/com/inventorysystem/util/HibernateUtil.java
@@ -1,5 +1,48 @@
 package com.inventorysystem.util;
 
-public class HibernateUtil {
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Load the configuration from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    /**
+     * Get the SessionFactory
+     *
+     * @return SessionFactory
+     */
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    /**
+     * Get a new Hibernate Session
+     *
+     * @return Session
+     */
+    public static Session getSession() {
+        return getSessionFactory().openSession();
+    }
+
+    /**
+     * Shutdown the SessionFactory
+     */
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
 }
+
