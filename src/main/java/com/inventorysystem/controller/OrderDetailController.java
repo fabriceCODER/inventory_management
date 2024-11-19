2f21d4f0bb93334f0fdd1bf19c3b5d4d14961c7e
FA <fabricecoder009@gmail.com>
2024-11-19 08:28:16 +0200

    creation and initialisation of OrderDetailController and OrderDetailDao classes

--- src/main/java/com/inventorysystem/controller/OrderDetailController.java
+++ src/main/java/com/inventorysystem/controller/OrderDetailController.java
@@ -1,7 +1,7 @@
 package com.inventorysystem.controller;
 
 import com.inventorysystem.dao.OrderDetailDao;
-import com.inventorysystem.model.OrderDetail;
+import com.inventorysystem.model.OrderDetails;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -12,5 +12,75 @@ public class OrderDetailController extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        orderDetailDao = new OrderDetailDao
+        orderDetailDao = new OrderDetailDao();
+    }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        try {
+            switch (action) {
+                case "new":
+                    showNewForm(request, response);
+                    break;
+                case "edit":
+                    showEditForm(request, response);
+                    break;
+                case "delete":
+                    deleteOrderDetail(request, response);
+                    break;
+                default:
+                    listOrderDetails(request, response);
+                    break;
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private void listOrderDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<OrderDetail> orderDetails = orderDetailDao.getAllOrderDetails();
+        request.setAttribute("orderDetails", orderDetails);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("orderDetail-list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("orderDetail-form.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        OrderDetail orderDetail = orderDetailDao.getOrderDetail(id);
+        request.setAttribute("orderDetail", orderDetail);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("orderDetail-form.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String quantityOrdered = request.getParameter("quantityOrdered");
+        String priceEach = request.getParameter("priceEach");
+
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setQuantityOrdered(Integer.parseInt(quantityOrdered));
+        orderDetail.setPriceEach(Double.parseDouble(priceEach));
+
+        if (id == null || id.isEmpty()) {
+            orderDetailDao.saveOrderDetail(orderDetail);
+        } else {
+            orderDetail.setOrderDetailId(Integer.parseInt(id));
+            orderDetailDao.updateOrderDetail(orderDetail);
+        }
+        response.sendRedirect("orderDetails");
+    }
+
+    private void deleteOrderDetail(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        orderDetailDao.deleteOrderDetail(id);
+        response.sendRedirect("orderDetails");
+    }
+}
